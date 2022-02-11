e2e52556bf599c4ab5db4170ecadc599aaa1cc05
Juho Lehtimäki <juho.a.lehtimaki@gmail.com>
2022-02-11 21:22:16 +0200

    Add ESLint & Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:jest/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': ['warn', { extensions: ['.tsx', '.ts'] }],
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+  },
+};
--- electron/main.ts
+++ electron/main.ts
@@ -1,8 +1,8 @@
-import { app, BrowserWindow } from "electron";
-import * as path from "path";
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
 import installExtension, {
   REACT_DEVELOPER_TOOLS,
-} from "electron-devtools-installer";
+} from 'electron-devtools-installer';
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -10,7 +10,7 @@ function createWindow() {
     height: 600,
     webPreferences: {
       // contextIsolation: false,
-      preload: path.join(__dirname, "preload.js"),
+      preload: path.join(__dirname, 'preload.js'),
     },
     autoHideMenuBar: true,
   });
@@ -19,22 +19,23 @@ function createWindow() {
     // 'build/index.html'
     win.loadURL(`file://${__dirname}/../index.html`);
   } else {
-    win.loadURL("http://localhost:3000/index.html");
+    win.loadURL('http://localhost:3000/index.html');
 
     win.webContents.openDevTools();
 
     // Hot Reloading on 'node_modules/.bin/electronPath'
-    require("electron-reload")(__dirname, {
+    // eslint-disable-next-line global-require
+    require('electron-reload')(__dirname, {
       electron: path.join(
         __dirname,
-        "..",
-        "..",
-        "node_modules",
-        ".bin",
-        "electron" + (process.platform === "win32" ? ".cmd" : "")
+        '..',
+        '..',
+        'node_modules',
+        '.bin',
+        `electron${process.platform === 'win32' ? '.cmd' : ''}`,
       ),
       forceHardReset: true,
-      hardResetMethod: "exit",
+      hardResetMethod: 'exit',
     });
   }
 }
@@ -43,18 +44,18 @@ app.whenReady().then(() => {
   // DevTools
   installExtension(REACT_DEVELOPER_TOOLS)
     .then((name) => console.log(`Added Extension:  ${name}`))
-    .catch((err) => console.log("An error occurred: ", err));
+    .catch((err) => console.log('An error occurred: ', err));
 
   createWindow();
 
-  app.on("activate", () => {
+  app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
   });
 
-  app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
       app.quit();
     }
   });
